61e5b12c5c6acb31f392c3628ec4fa544994d0e1
Matt-the-who <matthew.ho@ocsbstudent.ca>
2015-10-16 14:39:25 -0400

    Unicode Program

---
+++ UnicodeProgram/Unicode.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace UnicodeProgram
+{
+    partial class frmUnicodeProgram
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstBox = new System.Windows.Forms.ListBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstBox
+            // 
+            this.lstBox.FormattingEnabled = true;
+            this.lstBox.Location = new System.Drawing.Point(60, 86);
+            this.lstBox.Name = "lstBox";
+            this.lstBox.Size = new System.Drawing.Size(120, 95);
+            this.lstBox.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(82, 31);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // frmUnicodeProgram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lstBox);
+            this.Name = "frmUnicodeProgram";
+            this.Text = "Unicode program";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstBox;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ UnicodeProgram/Unicode.cs
@@ -0,0 +1,37 @@
+﻿/*
+Created by Matthew Ho
+Created on 10-16-15
+Created for ICS3U
+Daily Task 3-11
+This program converts characters to unicode and displays them in ASCII in a listbox.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnicodeProgram
+{
+    public partial class frmUnicodeProgram : Form
+    {
+        public frmUnicodeProgram()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            for(int loopCounter = 65; loopCounter != 91; loopCounter++)
+            {
+
+                this.lstBox.Items.Add(Char.ConvertFromUtf32(loopCounter) + " --> " + loopCounter);
+            }
+        }
+    }
+}
